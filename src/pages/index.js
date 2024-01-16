605c0f7f594f233d45de8085adb9095dd517e164
Mariglen <mcullhaj43@gmail.com>
2024-01-16 23:30:39 +0100

    adding ipapi country code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,41 @@
-
 export default function Home(props) {
-  const ip = props.ip;
-  const way = props.way;
+  const { ip, way, country } = props;
+
   return (
-    <div style={{ width: '100vw', height: '100vh', padding: 30 }}>
+    <div style={{ width: "100vw", height: "100vh", padding: 30 }}>
       <h1>
-        IP Address <span style={{ color: 'blue' }}>{ip}</span>
+        IP Address <span style={{ color: "blue" }}>{ip}</span>
       </h1>
       <h1>
-        Way of getting <span style={{ color: 'blue' }}>{way}</span>
+        Way of getting <span style={{ color: "blue" }}>{way}</span>
+      </h1>
+      <h1>
+        Country Code <span style={{ color: "blue" }}>{country}</span>
       </h1>
     </div>
   );
 }
 
 export async function getServerSideProps(context) {
-
   const { req } = context;
 
   let ip;
   let way;
-  if (req.headers['x-forwarded-for']) {
-    ip = req.headers['x-forwarded-for'].split(',')[0];
-    way = 'x-forwarded-for'
-  } else if (req.headers['x-real-ip']) {
+  if (req.headers["x-forwarded-for"]) {
+    ip = req.headers["x-forwarded-for"].split(",")[0];
+    way = "x-forwarded-for";
+  } else if (req.headers["x-real-ip"]) {
     ip = req.connection.remoteAddress;
-    way = 'x-real-ip'
+    way = "x-real-ip";
   } else {
     ip = req.connection.remoteAddress;
-    way = 'connection.remoteAddress'
+    way = "connection.remoteAddress";
   }
 
-  console.log(ip);
+  const response = await fetch(`https://ipapi.co/${ip}/country`);
+  const country = await response.text();
+
   return {
-    props: {ip,way },
+    props: { ip, way, country },
   };
 }
