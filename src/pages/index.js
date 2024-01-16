3c0ce43a846840061fe0eeb30ecb18b566fdd1a8
Mariglen <mcullhaj43@gmail.com>
2024-01-16 23:25:46 +0100

    add the way

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,38 @@
 
 export default function Home(props) {
   const ip = props.ip;
+  const way = props.way;
   return (
     <div style={{ width: '100vw', height: '100vh', padding: 30 }}>
       <h1>
         IP Address <span style={{ color: 'blue' }}>{ip}</span>
+      </h1>
+      <h1>
+        Way of getting <span style={{ color: 'blue' }}>{way}</span>
       </h1>
     </div>
   );
 }
 
 export async function getServerSideProps(context) {
+
   const { req } = context;
-  let ip =req.connection.remoteAddress
 
-
-  // if (req.headers['x-forwarded-for']) {
-  //   ip = req.headers['x-forwarded-for'].split(',')[0];
-  // } else if (req.headers['x-real-ip']) {
-  //   ip = req.connection.remoteAddress;
-  // } else {
-  //   ip = req.connection.remoteAddress;
-  // }
+  let ip;
+  let way;
+  if (req.headers['x-forwarded-for']) {
+    ip = req.headers['x-forwarded-for'].split(',')[0];
+    way = 'x-forwarded-for'
+  } else if (req.headers['x-real-ip']) {
+    ip = req.connection.remoteAddress;
+    way = 'x-real-ip'
+  } else {
+    ip = req.connection.remoteAddress;
+    way = 'connection.remoteAddress'
+  }
 
   console.log(ip);
   return {
-    props: {
-      ip,
-    },
+    props: {ip,way },
   };
 }
